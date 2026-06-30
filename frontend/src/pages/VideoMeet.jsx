a4618924daba32453eb15dde4cd27920328f42dc
Pranali Khot <khotpranali77@gmail.com>
2026-06-30 10:11:55 +0530

    Add error logging to debug offer/answer signaling

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -230,27 +230,30 @@ export default function VideoMeetComponent() {
         })
     }
 
-    let gotMessageFromServer = (fromId, message) => {
-        var signal = JSON.parse(message)
+let gotMessageFromServer = (fromId, message) => {
+    var signal = JSON.parse(message)
 
-        if (fromId !== socketIdRef.current) {
-            if (signal.sdp) {
-                connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(() => {
+    if (fromId !== socketIdRef.current) {
+        if (signal.sdp) {
+            connections[fromId].setRemoteDescription(new RTCSessionDescription(signal.sdp))
+                .then(() => {
                     if (signal.sdp.type === 'offer') {
                         connections[fromId].createAnswer().then((description) => {
                             connections[fromId].setLocalDescription(description).then(() => {
                                 socketRef.current.emit('signal', fromId, JSON.stringify({ 'sdp': connections[fromId].localDescription }))
-                            }).catch(e => console.log(e))
-                        }).catch(e => console.log(e))
+                            }).catch(e => console.log("Error setting local description:", e))
+                        }).catch(e => console.log("Error creating answer:", e))
                     }
-                }).catch(e => console.log(e))
-            }
+                })
+                .catch(e => console.log("Error setting remote description:", e))
+        }
 
-            if (signal.ice) {
-                connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice)).catch(e => console.log(e))
-            }
+        if (signal.ice) {
+            connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice))
+                .catch(e => console.log("Error adding ICE candidate:", e))
         }
     }
+}
 
 
 
