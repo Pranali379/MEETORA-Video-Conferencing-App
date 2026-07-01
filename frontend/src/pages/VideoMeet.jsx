18162515f9da00b8d4f814c64424bca63abe0d60
Pranali Khot <khotpranali77@gmail.com>
2026-07-01 10:38:41 +0530

    Switch to Metered TURN server

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -19,23 +19,28 @@ var connections = {};
 
 const peerConfigConnections = {
     "iceServers": [
-        { "urls": "stun:stun.l.google.com:19302" },
-        { "urls": "stun:stun1.l.google.com:19302" },
-        { "urls": "stun:stun2.l.google.com:19302" },
         {
-            "urls": "turn:openrelay.metered.ca:80",
-            "username": "openrelayproject",
-            "credential": "openrelayproject"
+            "urls": "stun:stun.relay.metered.ca:80"
         },
         {
-            "urls": "turn:openrelay.metered.ca:443",
-            "username": "openrelayproject",
-            "credential": "openrelayproject"
+            "urls": "turn:standard.relay.metered.ca:80",
+            "username": "57c0b947f8c0abe9ddb1f428",
+            "credential": "lFA16p16Q3IseEAe"
         },
         {
-            "urls": "turn:openrelay.metered.ca:443?transport=tcp",
-            "username": "openrelayproject",
-            "credential": "openrelayproject"
+            "urls": "turn:standard.relay.metered.ca:80?transport=tcp",
+            "username": "57c0b947f8c0abe9ddb1f428",
+            "credential": "lFA16p16Q3IseEAe"
+        },
+        {
+            "urls": "turn:standard.relay.metered.ca:443",
+            "username": "57c0b947f8c0abe9ddb1f428",
+            "credential": "lFA16p16Q3IseEAe"
+        },
+        {
+            "urls": "turns:standard.relay.metered.ca:443?transport=tcp",
+            "username": "57c0b947f8c0abe9ddb1f428",
+            "credential": "lFA16p16Q3IseEAe"
         }
     ]
 }
